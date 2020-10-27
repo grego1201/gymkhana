8f852578cb14dd3b70805db1a7b210a23fe85a21
grego1201 <gregoriopatinoesteo@gmail.com>
2020-10-27 17:59:50 +0100

    Add end quest text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_26_214033) do
+ActiveRecord::Schema.define(version: 2020_10_27_163541) do
 
   create_table "quests", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_10_26_214033) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "code"
     t.integer "next_quest_id"
+    t.string "end_quest_text"
   end
 
 end
